2b1c7a41e04384cd00bfeb7f7f6f08e03d99ad3c
the69nemo <thestigmata@yandex.ru>
2022-10-17 21:13:15 +0300

    some_adaptive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import './Hero.css';
-import '../../blocks/link-view/link-view.css'
-import HeroBg from '../../img/images/hero-bg.jpg';
+import React from "react";
+import "./Hero.css";
+import "../../blocks/link-view/link-view.css";
+import HeroBg from "../../img/images/hero-bg.jpg";
+import LinkView from "../../blocks/link-view/link-view";
 
 function Hero() {
   return (
-    <div className='hero'>
-      <div className='hero__container'>
-        <div className='hero__text-container'>
-          <div className='hero__text-wrapper'>
-            <h2 className='hero__text-title'>Luxury homeware for people who love timeless design quality</h2>
-            <p className='hero__text-subtitle'>Shop the new Spring 2022 collection today</p>
+    <div className="hero">
+      <div className="hero__container">
+        <div className="hero__text-container">
+          <div className="hero__text-wrapper">
+            <h2 className="hero__text-title">
+              Luxury homeware for people who love timeless design quality
+            </h2>
+            <p className="hero__text-subtitle">
+              Shop the new Spring 2022 collection today
+            </p>
           </div>
-          <a className='link-view' href='#'>View collection</a>
+          <LinkView text='View collection'/>
         </div>
-        <img src={HeroBg} className='hero__img' alt='hero img'/>
+        <div className="hero__img-container" />
       </div>
     </div>
-   );
+  );
 }
 
 export default Hero;
