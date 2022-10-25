8f49bcc645d4bd328e869434770467858d2b6aab
the69nemo <thestigmata@yandex.ru>
2022-10-25 21:17:01 +0300

    burger_menu

---
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BurgerMenu.css";
+import CartIcon from "../../img/icons/Shopping--cart.svg";
+import UserIcon from "../../img/icons/User--avatar.svg";
+
+function BurgerMenu({ isBurgerMenuOpen, handleBurgerMenuOpen }) {
+  return (
+    <div className={`burgerMenu ${isBurgerMenuOpen && 'open'} `}>
+      <div className="burgerMenu__container">
+        <div className="burgerMenu__top">
+          <ul className="burgerMenu__icons-container">
+            <li className="burgerMenu__icons-list">
+              <Link to="/basket">
+                <img src={CartIcon} alt="CartIcon" />
+              </Link>
+            </li>
+            <li className="burgerMenu__icons-list">
+              <img src={UserIcon} alt="UserIcon" />
+            </li>
+          </ul>
+          <button type="button" className="burgerMenu__btn-close" onClick={handleBurgerMenuOpen} />
+        </div>
+        <div className="burgerMenu__bottom">
+        <nav className="burgerMenu__nav">
+            <a href="#" className="burgerMenu__nav-link">
+              Plant pots
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Ceramics
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Tables
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Chairs
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Crockery
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Tableware
+            </a>
+            <a href="#" className="burgerMenu__nav-link">
+              Cutlery
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BurgerMenu;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import SearchIcon from "../../img/icons/Search.svg";
 import LogoIcon from "../../img/icons/logo.svg";
 import CartIcon from "../../img/icons/Shopping--cart.svg";
 import UserIcon from "../../img/icons/User--avatar.svg";
-
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 function Header() {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const handleBurgerMenuOpen = () => setIsBurgerMenuOpen(!isBurgerMenuOpen);
+
   return (
     <header className="header">
       <div className="header__container">
-        <div className="header__logo-container">
-          <img src={SearchIcon} alt="SearchIcon" />
-          <Link to="/">
-            <img src={LogoIcon} alt="LogoIcon" />
-          </Link>
-          <ul className="header__icons-container">
-            <li className="header__icons-list">
-              <Link to="/basket">
-                <img src={CartIcon} alt="CartIcon" />
-              </Link>
-            </li>
-            <li className="header__icons-list">
-              <img src={UserIcon} alt="UserIcon" />
-            </li>
-          </ul>
+        <div className="header__top">
+          <div className="header__logo-container">
+            <img src={SearchIcon} alt="SearchIcon" />
+            <Link to="/">
+              <img src={LogoIcon} alt="LogoIcon" />
+            </Link>
+            <ul className="header__icons-container">
+              <li className="header__icons-list">
+                <Link to="/basket">
+                  <img src={CartIcon} alt="CartIcon" />
+                </Link>
+              </li>
+              <li className="header__icons-list">
+                <img src={UserIcon} alt="UserIcon" />
+              </li>
+            </ul>
+            <button type="button" className="header__burger-btn" onClick={handleBurgerMenuOpen} />
+          </div>
         </div>
-        <div className="header__border-line" />
-        <nav className="header__nav">
-          <a href="#" className="header__nav-link">
-            Plant pots
-          </a>
-          <a href="#" className="header__nav-link">
-            Ceramics
-          </a>
-          <a href="#" className="header__nav-link">
-            Tables
-          </a>
-          <a href="#" className="header__nav-link">
-            Chairs
-          </a>
-          <a href="#" className="header__nav-link">
-            Crockery
-          </a>
-          <a href="#" className="header__nav-link">
-            Tableware
-          </a>
-          <a href="#" className="header__nav-link">
-            Cutlery
-          </a>
-        </nav>
+        <div className="header__bottom">
+          <nav className="header__nav">
+            <a href="#" className="header__nav-link">
+              Plant pots
+            </a>
+            <a href="#" className="header__nav-link">
+              Ceramics
+            </a>
+            <a href="#" className="header__nav-link">
+              Tables
+            </a>
+            <a href="#" className="header__nav-link">
+              Chairs
+            </a>
+            <a href="#" className="header__nav-link">
+              Crockery
+            </a>
+            <a href="#" className="header__nav-link">
+              Tableware
+            </a>
+            <a href="#" className="header__nav-link">
+              Cutlery
+            </a>
+          </nav>
+        </div>
       </div>
+      <BurgerMenu
+        isBurgerMenuOpen = {isBurgerMenuOpen}
+        handleBurgerMenuOpen = {handleBurgerMenuOpen}
+      />
     </header>
   );
 }
