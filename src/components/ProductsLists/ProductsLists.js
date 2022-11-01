7240278abd0d8f832aa78a48da27d8df795ef030
the69nemo <thestigmata@yandex.ru>
2022-11-01 20:55:18 +0300

    some_fix_render_product

--- src/components/ProductsLists/ProductsLists.js
+++ src/components/ProductsLists/ProductsLists.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./ProductsLists.css";
 import ProductCard from "../ProductCard/ProductCard";
 
 function ProductsLists({ allData }) {
-  
-  const cards = allData.map((card) => (
+
+  const addCards = 9;
+
+  const [currentCards, setCurrentCard] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState([]);
+  const [hiddenButton, setHiddenButton] = useState(false);
+
+  const renderAddCards = useCallback(() => {
+    const count = Math.min(allData.length, currentCards + addCards);
+    const moreCards = allData.slice(currentCards, count);
+    setCardsToShow([...cardsToShow, ...moreCards]);
+    setCurrentCard(count);
+  }, [currentCards, allData, cardsToShow])
+
+  useEffect(() => {
+    const count = Math.min(allData.length, addCards);
+    setCardsToShow(allData.slice(0, count));
+    setCurrentCard(count);
+  }, [allData]);
+
+  useEffect(() => {
+    if (currentCards > allData.length || currentCards == allData.length) {
+      setHiddenButton(true);
+    }
+  }, [currentCards])
+
+  const cards = cardsToShow.map((card) => (
     <ProductCard
-      key={card.id}
+      key={Math.random() * Date.now()}
       title={card.name}
       alt={card.name}
       src={card.img}
@@ -21,7 +46,7 @@ function ProductsLists({ allData }) {
         {cards}
       </div>
       <div className="productsLists__button-wrapper">
-        <button className="productsLists__button" type="button">
+        <button className={`productsLists__button ${hiddenButton && 'productsLists__button_hidden'}`} type="button" onClick={renderAddCards}>
           Load more
         </button>
       </div>
